45ea7e023ba5f089fe80687ad7d6a922b2bdeb21
Mihir Patel <ee.mihir.patel@gmail.com>
2016-09-01 09:17:30 -0400

    outline distinct blocks

--- src/components/grid-cell.js
+++ src/components/grid-cell.js
@@ -3,9 +3,15 @@ import React, { PropTypes } from 'react';
 class GridCell extends React.Component {
   render () {
     let displayValue = this.props.value || 0;
+    let {extendsTop, extendsBottom, extendsLeft, extendsRight} = this.props;
     let classes = [
       "grid__cell"
       , "tile-" + (displayValue)
+      , displayValue ? 'grid__cell--no-shadow' : ''
+      , extendsTop ? 'grid__cell--extends-top' : ''
+      , extendsBottom ? 'grid__cell--extends-bottom' : ''
+      , extendsLeft ? 'grid__cell--extends-left' : ''
+      , extendsRight ? 'grid__cell--extends-right' : ''
     ];
     return (
       <div className={classes.join(' ')}>
